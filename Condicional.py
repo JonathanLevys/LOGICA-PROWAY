06c050f3b6127d2b52fbb0df5d1b18a2cf130a5e
Jonathan Levy <98614904+JonathanLevys@users.noreply.github.com>
2023-10-18 20:25:38 -0300

    Update Condicional.py
    Uso do len

--- Condicional.py
+++ Condicional.py
@@ -22,3 +22,23 @@ elif nota >= 3:
     print('substitutiva')
 else:
     print('reprovado')
+----------------------------------------------------
+
+
+nome = 'Jonathan'
+num_caract = len(nome)
+
+tamanho pequeno até 5 caracteres
+tamanho médio até 7 caracteres
+tamanho grande até 10 caracteres
+tamanho gigante acima 10 caracteres
+
+
+if num_caract < 6:
+     print('pequeno')
+ elif num_caract < 8:
+     print('medio')
+ elif num_caract < 11:
+     print('grande')
+else:
+    print('gigante')
